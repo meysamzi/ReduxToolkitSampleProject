b97b91b5151437cfc98c5ed8315bc10cf131d453
meysamzi <meysamzeynali2@gmail.com>
2023-04-16 02:29:03 +0430

    Display Data Completed with a Poor UI :D

--- src/Components/TodoCards.js
+++ src/Components/TodoCards.js
@@ -1,7 +1,32 @@
-import { Card } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 
 const TodoCards = ({ data }) => {
-  return <Card sx={{ padding: "1rem" }}>asdsad</Card>;
+  return (
+    <Card
+      sx={{
+        padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      {Object.keys(data).map((objectKey) => (
+        <Box
+          key={objectKey.id}
+          sx={{
+            display: "flex",
+            //   justifyContent: "space-between",
+            gap: "1rem",
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold" }}> {objectKey}</Typography>
+          <Typography sx={{ textAlign: "justify" }}>
+            {data[objectKey]}
+          </Typography>
+        </Box>
+      ))}
+    </Card>
+  );
 };
 
 export default TodoCards;
